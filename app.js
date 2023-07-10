294f11fa2e4620750182b130c36dbd4ef3645e61
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-10 10:36:11 +0200

    DONE WITH LECTURE

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-console.log(document.getElementById("pTag"))
+// SELECTORS 
 const pTag = document.querySelector('#pTag')
+const parent = document.querySelector('#parent')
 
+// ADD EVENT LISTENER 
+// https://www.w3schools.com/jsref/met_element_addeventlistener.asp
+// LIST OF DIFFERENT EVENTS
+// https://www.w3schools.com/jsref/dom_obj_event.asp
 // document.getElementById("pTag").addEventListener('click', ()=>alert("Hey don't touch that"))
 // document.querySelector("#pTag").innerHTML = "We have changed the p tag"
+
 
 const handleClick = () => {
     pTag.innerHTML = "Hey you clicked the P tag";
@@ -19,4 +25,21 @@ const handleClick = () => {
 
 pTag.addEventListener('click', handleClick)
 
-console.log(document.getElementById("pTag"))
+// console.log(document.getElementsByTagName('p'))
+// console.log(document.getElementsByClassName('others'))
+// console.log(document.querySelectorAll('p.others'));
+
+// console.log(document.querySelector('#parent').lastElementChild)
+// document.removeChild(pTag)
+
+const appendNewPTag = () => {
+    console.log(parent);
+
+    const para = document.createElement("p");
+    para.innerText = "This is a new paragraph";
+    parent.appendChild(para);
+}
+
+document.querySelector("#main").addEventListener('click', appendNewPTag)
+
+// console.log(document.getElementById("pTag"))

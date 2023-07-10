6dce9f250aaa111107a36cb72663f811fab18068
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-10 10:04:42 +0200

    Second example

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 console.log(document.getElementById("pTag"))
+const pTag = document.querySelector('#pTag')
 
 // document.getElementById("pTag").addEventListener('click', ()=>alert("Hey don't touch that"))
 // document.querySelector("#pTag").innerHTML = "We have changed the p tag"
 
 const handleClick = () => {
-    console.log("Hey you clicked the P tag");
-    document.querySelector('#pTag').style.backgroundColor = "red"
+    pTag.innerHTML = "Hey you clicked the P tag";
+    // pTag.style.backgroundColor = "red"
+    console.log(pTag.style.backgroundColor);
+
+    // if(pTag.style.backgroundColor == "red"){
+    //     pTag.style.backgroundColor = ""
+    // }else{
+    //     pTag.style.backgroundColor = "red"
+    // }
+    pTag.style.backgroundColor == "red" ? pTag.style.backgroundColor = "" : pTag.style.backgroundColor = "red"
 }
 
-document.querySelector("#pTag").addEventListener('click', handleClick)
+pTag.addEventListener('click', handleClick)
 
 console.log(document.getElementById("pTag"))

3a70454952157d1d3aa354470edd0685a7e2226e
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-11 10:26:28 +0200

    Done with local storage

--- local.js
+++ local.js
@@ -1,10 +1,11 @@
 // https://www.w3schools.com/jsref/prop_win_localstorage.asp
+// https://www.geeksforgeeks.org/how-to-store-an-array-in-localstorage/
 
 // SELECTORS
 const list = document.querySelector('#list')
 const input = document.querySelector('#specialInput')
 const form = document.querySelector('#specialForm')
-
+const megaDestroy = document.querySelector('#megaDestroy')
 
 
 // HANDLER FUNCTIONS
@@ -21,32 +22,26 @@ const handleSubmit = (e) => {
         alert('Input is empty')
     }else{
         loadItems(input.value)
-        
-        console.log(localStorage.getItem("itemsArr"))
-
         if(localStorage.getItem("itemsArr") == null){
             const newArr = [input.value]
             input.value = ''
             localStorage.setItem("itemsArr", JSON.stringify(newArr));
         }else{
-        const itemsArr = JSON.parse(localStorage.getItem("itemsArr"))
-        console.log(itemsArr)
-        itemsArr.push(input.value)
-
-        localStorage.setItem("itemsArr", JSON.stringify(itemsArr));
-
-        console.log(localStorage.getItem("itemsArr"))
-        
-        input.value = ''
+            const itemsArr = JSON.parse(localStorage.getItem("itemsArr"))
+            itemsArr.push(input.value)
+            localStorage.setItem("itemsArr", JSON.stringify(itemsArr));
+            input.value = ''
         }
-
     }
-    
 }
 
 
 // EVENT LISTNER
 form.addEventListener('submit', handleSubmit)
+megaDestroy.addEventListener('click', ()=>{
+    localStorage.clear()
+    list.innerHTML = ''
+})
 
 if(localStorage.getItem("itemsArr") == null){
     console.log("no items stored")
